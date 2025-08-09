c02da235fa776af4bb8aacf5fce7ffb991598751
nameismani <manikandan@i2global.io>
2025-08-09 17:21:03 +0530

    did some changes

--- client/src/app/(private)/chat/page.tsx
+++ client/src/app/(private)/chat/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import ChatSidebar from "@/components/chat/ChatSidebar";
-import ChatWindow from "@/components/chat/ChatWindow";
+import ChatContainer from "@/components/chat/ChatContainer";
 import { ChatProvider } from "@/providers/chat.provider";
 
 export default function ChatPage() {
     return (
         <ChatProvider>
-            <div className="flex h-[calc(100vh-64px)]">
-                <div className="w-80 h-full">
-                    <ChatSidebar />
-                </div>
-                <div className="flex-1 h-full">
-                    <ChatWindow />
-                </div>
-            </div>
+            <ChatContainer />
         </ChatProvider>
     );
 }
---
+++ client/src/components/chat/ChatContainer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ChatWindow from './ChatWindow'
+import ChatSidebar from './ChatSidebar'
+
+const ChatContainer = () => {
+    return (
+        <div className="flex h-[calc(100vh-64px)]">
+            <div className="w-80 h-full">
+                <ChatSidebar />
+            </div>
+            <div className="flex-1 h-full">
+                <ChatWindow />
+            </div>
+        </div>
+    )
+}
+
+export default ChatContainer
